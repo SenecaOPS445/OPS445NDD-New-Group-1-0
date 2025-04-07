61886b6c7056c3650cb8ecacaf4c92be0a37236e
Samip Paudel <spaudel11@myseneca.ca>
2025-04-06 21:55:29 -0400

    added search function

--- assignment2.py
+++ assignment2.py
@@ -7,9 +7,10 @@ import argparse
 # Function to list running processes
 def list_processes():
 
-
 # Function to search for a process by name
 def search_process(name):
+    print(f"Searching for process with name : {name}")
+    
 
 # Function to terminate a process by PID
 def terminate_process(pid):
