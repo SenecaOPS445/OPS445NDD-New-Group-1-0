2aa4119120ea49c8456f401bd7d2139d0bee8a40
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-04-06 21:18:01 -0400

    assignment2 template

---
+++ assignmet2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import argparse
+
+# Function to list running processes
+def list_processes():
+
+
+# Function to search for a process by name
+def search_process(name):
+
+# Function to terminate a process by PID
+def terminate_process(pid):
+
+if __name__ == "__main__":
