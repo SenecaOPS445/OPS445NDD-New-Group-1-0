f20c21eaccb4fb7d5e7402c7e82bd182c0a0403a
Samip Paudel <spaudel11@myseneca.ca>
2025-04-06 21:10:07 -0400

    template for assign2

---
+++ assignment2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" This assignment is prepared by ALI, SAMIP and LOGAN for OPS445.
+SEC: NDD
+"""
+
+import os
+import argparse
+
+# Function to list running processes
+def list_processes():
+
+
+# Function to search for a process by name
+def search_process(name):
+
+# Function to terminate a process by PID
+def terminate_process(pid):
+
+if __name__ == "__main__":
