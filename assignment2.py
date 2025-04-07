7eadf0631747c4b4185698ac63bc5c723b3e0892
Samip Paudel <spaudel11@myseneca.ca>
2025-04-06 21:57:29 -0400

    added search function

--- assignment2.py
+++ assignment2.py
@@ -10,7 +10,7 @@ def list_processes():
 # Function to search for a process by name
 def search_process(name):
     print(f"Searching for process with name : {name}")
-    
+    os.system(f'ps aux | grep {name}')
 
 # Function to terminate a process by PID
 def terminate_process(pid):
