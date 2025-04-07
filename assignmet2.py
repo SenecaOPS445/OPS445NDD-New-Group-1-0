56c792c298036ddb4c3652f7e6a0a8ff341e7d3a
Ali Ahmad Faiz Ahmad <afaiz-ahmad@myseneca.ca>
2025-04-06 21:47:26 -0400

    Added kill function

--- assignmet2.py
+++ assignmet2.py
@@ -11,7 +11,9 @@ def list_processes():
 # Function to search for a process by name
 def search_process(name):
 
-# Function to terminate a process by PID
-def terminate_process(pid):
+# Ali: Function to terminate a process by PID
+def terminate_process(pid): # defines a function which takes the proccess ID as in put
+    print(f"Terminating process with PID: {pid}") # print the following message to the user
+    os.system(f'kill {pid}') # after running the "kill" command, it stops the process with that PID
 
 if __name__ == "__main__":
